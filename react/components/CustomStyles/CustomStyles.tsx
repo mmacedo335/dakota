1c684ee5093a924d4f8c27aae589d10ce9ab0959
Matheus Macedo <matheus.macedo@agenciagh.com.br>
2024-06-27 09:08:54 -0300

    primeiro commit

---
+++ react/components/CustomStyles/CustomStyles.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//@ts-ignore 
+import styles from './CustomStyles.css'
+
+const CustomStyles = () => {
+  return (
+    <div className={styles.CustomStyles}></div>
+  )
+} 
+  
+export default CustomStyles
+ 
