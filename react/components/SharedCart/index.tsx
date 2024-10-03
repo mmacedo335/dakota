550fbdc7ab6e591b69755ec7398ea08678b34b05
Matheus Macedo <mmacedo335@gmail.com>
2024-10-03 14:02:41 -0300

    página coleções

--- react/components/SharedCart/index.tsx
+++ react/components/SharedCart/index.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect } from "react";
 import { OrderForm } from "vtex.order-manager";
-
 const SharedCart: React.FC = () => {
   const currentLocation = window.location.pathname;
   const { useOrderForm } = OrderForm;
   const { setOrderForm } = useOrderForm();
-
   // Função para definir o cookie a partir do backend com limite de tentativas
   async function setCookieFromBackend(retries = 5) {
     try {
       const response = await fetch("/_v/cookie", {
         method: "POST",
       });
-
       if (response.status === 404 || response.status === 500) {
         // Tentar novamente até esgotar o número de tentativas
         if (retries > 0) {
@@ -29,14 +26,12 @@ const SharedCart: React.FC = () => {
       console.error("Erro ao fazer requisição:", error);
     }
   }
-
   // Função para limpar o cookie a partir do backend com limite de tentativas
   async function setCookieClearFromBackend(retries = 5) {
     try {
       const response = await fetch("/_v/cookieclear", {
         method: "POST",
       });
-
       if (response.status === 404 || response.status === 500) {
         // Tentar novamente até esgotar o número de tentativas
         if (retries > 0) {
@@ -49,7 +44,6 @@ const SharedCart: React.FC = () => {
       console.error("Erro ao limpar cookie:", error);
     }
   }
-
   useEffect(() => {
     // Função para realizar as ações de cookies
     const handleCookieActions = async () => {
@@ -59,12 +53,10 @@ const SharedCart: React.FC = () => {
         await setCookieFromBackend();
       }
     };
-
     // Adicionando um listener para garantir que a lógica só seja executada após a página estar totalmente carregada
     const onLoadHandler = () => {
       handleCookieActions();
     };
-
     // Garantindo que a lógica só execute quando a página estiver carregada
     if (document.readyState === "complete") {
       // Se a página já estiver carregada, executa imediatamente
@@ -73,14 +65,11 @@ const SharedCart: React.FC = () => {
       // Caso contrário, espera o evento de load
       window.addEventListener("load", onLoadHandler);
     }
-
     // Remover o listener no cleanup
     return () => {
       window.removeEventListener("load", onLoadHandler);
     };
   }, [currentLocation]);
-
   return null;
 };
-
 export default SharedCart;
