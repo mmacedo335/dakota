32c37cb542a92253f221bc0546f60f545162e616
Gabriel Sabatini <g.sabatini@agenciafg.com.br>
2024-12-05 11:22:16 -0300

    ajustes minicart2

--- react/components/MiniCartCustom/FooterMinicart.tsx
+++ react/components/MiniCartCustom/FooterMinicart.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 //@ts-ignore
 import { OrderForm } from "vtex.order-manager";
 
@@ -13,16 +13,15 @@ function FooterMiniCart() {
     const { useOrderForm } = OrderForm;
     const OrderFormContext = useOrderForm();
 
-    // Garantindo valores padrão para evitar erros
     const totalValue = OrderFormContext?.orderForm?.totalizers?.[0]?.value ?? 0;
     const discountValue = totalValue * valueDiscountPix;
     const finalValue = totalValue - discountValue;
 
     const installmentOptions =
         OrderFormContext?.orderForm?.paymentData?.installmentOptions?.[0]?.installments ?? [];
-    const quantityParcel = installmentOptions.length > 0 ? installmentOptions.length : 1;
+    const quantityParcel = installmentOptions.length;
 
-    const priceParcel = quantityParcel > 0 ? totalCart / quantityParcel : totalCart;
+    const priceParcel = quantityParcel > 0 ? totalCart / quantityParcel : 0;
 
     function formatCurrency(value: number) {
         const valor = value / 100;
@@ -51,11 +50,13 @@ function FooterMiniCart() {
                 <span>Total</span> <span>{formatCurrency(totalCart)}</span>
             </div>
 
-            <div className={styles.FooterMiniCartInstallments}>
-                <span>
-                    Em até {quantityParcel}x de {formatCurrency(priceParcel)}
-                </span>
-            </div>
+            {quantityParcel > 0 && (
+                <div className={styles.FooterMiniCartInstallments}>
+                    <span>
+                        Em até {quantityParcel}x de {formatCurrency(priceParcel)}
+                    </span>
+                </div>
+            )}
 
             <div className={styles.FooterMiniCartPriceDiscount}>
                 ou <strong>{formatCurrency(finalValue)}</strong> à vista no PIX
