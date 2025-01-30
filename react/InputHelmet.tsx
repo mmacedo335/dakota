0e691caa0c0e41fa2c275e3ff1efc0de9be3031b
Gabriel Mancin <gabriel.mancin@agenciafg.com.br>
2025-01-30 09:16:28 -0300

    Title categorias

---
+++ react/InputHelmet.tsx
@@ -0,0 +1,2 @@
+import InputHelmet from "./components/InputHelmet/InputHelmet";
+export default InputHelmet;
---
+++ react/components/InputHelmet/InputHelmet.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Helmet, useRuntime } from 'vtex.render-runtime';
+
+function InputHelmet() {
+  const { page, account, route } = useRuntime();
+  const isDepartmentPage = page === 'store.search#department';
+  const departmentName = route?.params?.department;
+
+  const [titlePagina, setTitlePagina] = useState('');
+
+  useEffect(() => {
+    const handlePageLoad = () => {
+      console.log("departamento: ", departmentName);
+      console.log("route: ", route);
+      const element = document.querySelector('.vtex-rich-text-0-x-paragraph--title-da-pagina');
+      if (element) {
+        setTitlePagina(element.textContent);
+        console.log('Título da página customizado:', element.textContent);
+      } else {
+        console.log('Não foi encontrado title customizado.');
+        setTitlePagina('');
+      }
+    };
+
+
+    if (departmentName) {
+      handlePageLoad();
+    }
+  }, [departmentName]);
+
+  return (
+    <>
+      {titlePagina && (
+        <Helmet>
+          <title>{titlePagina}</title>
+        </Helmet>
+      )}
+    </>
+  );
+}
+
+export default InputHelmet;
