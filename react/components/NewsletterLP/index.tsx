20dc266e4468b3bf5eb72dc2da329691dc71637a
lucasastinofg <lucas.astino@agenciafg.com.br>
2025-03-10 18:17:02 -0300

    ajustando newsletter

--- react/components/NewsletterLP/index.tsx
+++ react/components/NewsletterLP/index.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import { useCssHandles } from "vtex.css-handles";
-import './style.css'
+import "./style.css";
 
 const CSS_form = [
-    "container__newslatter-lp",
-    "container__newslatter-title",
-    "container__newslatter-input",
-    "container__newslatter-input--container",
-    "container__newslatter-form",
-    "container__newslatter-button"
-  ] as const;
+  "container__newslatter-lp",
+  "container__newslatter-lp--wrapper",
+  "container__newslatter-title",
+  "container__newslatter-input",
+  "container__newslatter-input--container",
+  "container__newslatter-form",
+  "container__newslatter-button",
+  "container__newslatter-message",
+] as const;
 
 const NewsletterFormLp = () => {
   const [email, setEmail] = useState("");
   const [isSuccessVisible, setIsSuccessVisible] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-    const { handles } = useCssHandles(CSS_form);
+  const { handles } = useCssHandles(CSS_form);
 
   const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
     if (email.trim() === "") {
       setErrorMessage("Por favor, insira um e-mail válido.");
@@ -26,55 +28,80 @@ const NewsletterFormLp = () => {
     }
 
     const formData = {
-      email: email,
-      modal: true
+      email,
     };
 
     try {
-      const response = await fetch("/api/dataentities/--/documents", {
-        method: "POST",
+      const response = await fetch("/api/dataentities/NL/documents", {
+        method: "PATCH", // Alterado de POST para PATCH para evitar duplicatas
         headers: {
           "Content-Type": "application/json",
+          Accept: "application/json",
+          "REST-Range": "resources=0-1", // Para evitar problemas de paginação
+          // "X-VTEX-API-AppKey": "SUA_APP_KEY",
+          // "X-VTEX-API-AppToken": "SEU_APP_TOKEN"
         },
         body: JSON.stringify(formData),
       });
 
       if (response.ok) {
         setIsSuccessVisible(true);
-        setErrorMessage(""); 
+        setErrorMessage("");
         localStorage.setItem("newsletter", "ativo");
-        setEmail(""); 
+        setEmail("");
       } else {
-        setErrorMessage("Erro ao enviar o formulário. Tente novamente.");
+        const errorData = await response.json();
+        setErrorMessage(`Erro: ${errorData.message || "Tente novamente"}`);
       }
     } catch (error) {
+      console.error("Erro ao conectar ao servidor:", error);
       setErrorMessage("Erro ao conectar ao servidor.");
     }
   };
 
   return (
-    <div className={handles["container__newslatter-lp"]} >
-      <h3 className={handles["container__newslatter-title"]}>Inscreva-se e receba<br/>
-      <strong>5% OFF</strong> na primeira compra</h3>
-      <form className={handles["container__newslatter-form"]} onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "10px"}}>
-      <div className={handles["container__newslatter-input--container"]}>
-        <input
-        className={handles["container__newslatter-input"]}
-        type="email"
-        placeholder="Insira seu e-mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        />
-        </div>
-        <button className={handles["container__newslatter-button"]} type="submit" >
-        Eu quero
-        </button>
-      </form>
-
-      {isSuccessVisible && <p style={{ color: "green", marginTop: "10px" }}>Cadastro realizado com sucesso!</p>}
-      {errorMessage && <p style={{ color: "red", marginTop: "10px" }}>{errorMessage}</p>}
-    </div>
+    <section className={handles["container__newslatter-lp--wrapper"]}>
+      <div className={handles["container__newslatter-lp"]}>
+        <h3 className={handles["container__newslatter-title"]}>
+          Inscreva-se e receba
+          <br />
+          <strong>5% OFF</strong> na primeira compra
+        </h3>
+        <form
+          className={handles["container__newslatter-form"]}
+          onSubmit={handleSubmit}
+          style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+        >
+          <div className={handles["container__newslatter-input--container"]}>
+            <input
+              className={handles["container__newslatter-input"]}
+              type="email"
+              placeholder="Insira seu e-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            className={handles["container__newslatter-button"]}
+            type="submit"
+          >
+            Eu quero
+          </button>
+        </form>
+      </div>
+      {isSuccessVisible && (
+          <p
+            className={handles["container__newslatter-message"]}
+            style={{ color: "green", marginTop: "10px" }}
+          >
+            Cadastro realizado com sucesso!
+          </p>
+        )}
+        {errorMessage && (
+          <p style={{ color: "red", marginTop: "10px" }}>{errorMessage}</p>
+        )}
+    </section>
   );
 };
 
