4104bf7d259c3d345827cdf9b646f82189c98d47
Gabriel Sabatini <g.sabatini@agenciafg.com.br>
2025-08-26 13:58:43 -0300

    fix: correction in sku selection pdp

--- react/components/UltimasUnidades/index.tsx
+++ react/components/UltimasUnidades/index.tsx
@@ -6,38 +6,37 @@ import { useProduct } from 'vtex.product-context'
 const UltimasUnidades = () => {
     const productContext = useProduct()
     const product = productContext?.selectedItem
-
     const [availableQty, setAvailableQty] = useState<number | null>(null)
-
-    // Efeito para clicar automaticamente na variação correta
-    useEffect(() => {
-        if (!product?.name) return
-
-        // Pequeno delay para garantir que os elementos estejam renderizados
-        setTimeout(() => {
-            const elements = document.querySelectorAll('.vtex-store-components-3-x-skuSelectorItemTextValue')
-            elements.forEach((element) => {
-                if (element instanceof HTMLElement && element.textContent?.trim() === product.name) {
-                    element.click()
-                }
-            })
-
-            //console.log('Clicou na variação correta:', product.name)
-            
-        }, 1000)
-    }, [])
+    const [cont, setCont] = useState(0)
 
     useEffect(() => {
         const qty = product?.sellers?.[0]?.commertialOffer?.AvailableQuantity
-
         if (qty !== undefined) {
-            setAvailableQty(qty)
+            setAvailableQty(qty);
+        }
+        setCont(prev => prev + 1);
+        //console.log('cont', cont, product);
+    }, [product])
+
+    useEffect(() => {
+        if (typeof document !== 'undefined') {
+            const tamanhos = document.querySelectorAll<HTMLDivElement>(
+                '.vtex-store-components-3-x-skuSelectorItem:not(.vtex-store-components-3-x-unavailable)'
+            )
+            const handleClick = () => {
+                setCont(2);
+            }
+            tamanhos.forEach(el => el.addEventListener('click', handleClick))
+            // Cleanup
+            return () => {
+                tamanhos.forEach(el => el.removeEventListener('click', handleClick))
+            }
         }
     }, [product])
 
     return (
         <>
-            {availableQty !== null && availableQty <= 3 && (
+            {availableQty !== null && availableQty <= 3 && cont > 1 && (
                 <div className={styles.ultimasUnidades}>
                     <span className={styles.ultimas}>últimas unidades</span>
                     <span className={styles.disponível}>
@@ -45,11 +44,10 @@ const UltimasUnidades = () => {
                             ? 'Resta 1 item em estoque'
                             : `Restam ${availableQty} itens em estoque`}
                     </span>
-
                 </div>
             )}
         </>
     )
 }
 
-export default UltimasUnidades
+export default UltimasUnidades;
