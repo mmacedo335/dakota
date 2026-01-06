c80e8cae9fbe615b3fe1f23adfbf6b913aa4cf25
Daniel Carvalho <daniel.gdc22@gmail.com>
2026-01-06 11:09:43 -0300

    feat: custom login code acess by time

--- apps/auth-utils/react/components/LoggedModal.tsx
+++ apps/auth-utils/react/components/LoggedModal.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useRuntime } from "vtex.render-runtime";
 import { useCssHandles } from "vtex.css-handles";
 const CSS_HANDLES = ["LoggedModalWrapper","loggedModalOption"] as const;
 
@@ -7,12 +6,11 @@ const options = ["/account","/logout"];
 
 const LoggedModal = () => {
     const { handles } = useCssHandles(CSS_HANDLES);
-    const { navigate } = useRuntime();
 
     return <div className={handles.LoggedModalWrapper}>
             {
                 options.map((option,index) => {
-                    return <button className={handles.loggedModalOption} key={index} onClick={() => navigate({ to: option })}>{option === "/account" ? "Minha Conta" : "Sair"}</button>
+                    return  <a href={option} className={handles.loggedModalOption} target="_self" key={`${option}-${index}`} title={option}>{option === "/account" ? "Minha Conta" : "Sair"}</a>
                 })
             }
     </div>;
